d8296e60d851a0ef3afc15a1b0f7bc95a0dba76f
georgemmp <machadogeorge.p@gmail.com>
2019-08-10 20:55:35 -0300

    Implementação de likes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const DevController = require('./controller/dev.controller');
+const LikeController = require('./controller/like.controller');
 
 const routes = express.Router();
 
 routes.post('/dev', DevController.store);
+routes.post('/dev/:devId/like', LikeController.store);
 
 module.exports = routes;
