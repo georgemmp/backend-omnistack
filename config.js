f0b70911f45fa7c62e88850da878ef6cfed648fd
georgemmp <machadogeorge.p@gmail.com>
2019-08-10 17:50:00 -0300

    Configurando conexão com banco de dados

---
+++ config.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const env = {
+    DB_CONFIG: process.env.DB_CONFIG
+};
+
+module.exports = env;
+
--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
+const env = require('../config');
 
 const server = express();
 
-server.use(express.json());
+mongoose.connect(env.DB_CONFIG, { useNewUrlParser: true });
 
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
