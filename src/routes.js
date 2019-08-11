924a728ced3a3b918b5ad17147ba3a42424d80e5
georgemmp <machadogeorge.p@gmail.com>
2019-08-10 21:07:38 -0300

    Implementação de dislikes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const DevController = require('./controller/dev.controller');
 const LikeController = require('./controller/like.controller');
+const DislikeController = require('./controller/dislike.controller');
 
 const routes = express.Router();
 
 routes.post('/dev', DevController.store);
 routes.post('/dev/:devId/like', LikeController.store);
+routes.post('/dev/:devId/dislike', DislikeController.store);
 
 module.exports = routes;
